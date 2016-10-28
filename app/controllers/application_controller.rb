af0441ea5beabf27664d499efd6cdb66c557be76
Ly Duc Long <longlyduc@gmail.com>
2016-10-28 11:05:01 +0000

    change file to print goodbye world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #new method for hello app
-  def hello
-  	render text: "¡Hola, mundo!"
+  def goodbye
+  	render text: "goodbye world"
   end
 end
